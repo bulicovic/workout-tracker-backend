1bb187287a854ad806bb4b88f13ebcb45d762449
danielbulic <info@danielbulic.dev>
2024-09-12 21:24:06 +0800

    fix merge conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
 	kotlin("jvm") version "2.0.20"
 	kotlin("plugin.spring") version "2.0.20"
-	kotlin("jvm") version "2.0.20"
-	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 	kotlin("plugin.jpa") version "2.0.20"
